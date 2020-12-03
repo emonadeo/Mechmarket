345e438b3d7e991782281ca3523f373ac55c9847
Emanuel Pilz <emonadeo@gmail.com>
2020-12-03 15:56:30 +0100

    Migrated pictures function to analyser module

---
+++ src/util/analyser.js
@@ -0,0 +1,62 @@
+async function getPictures(text) {
+    if (!text) return [];
+
+    // get links in post and avoid duplicates
+    let pictures = [
+        ...new Set(
+            [
+                ...text.matchAll(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]*\.[a-zA-Z0-9]*\b([-a-zA-Z0-9@:%_+.~#/]*)/g),
+            ].map((res) => res[0])
+        ),
+    ];
+
+    // cancel if no pictures
+    if (!pictures) return {};
+
+    pictures = await Promise.all(
+        pictures.map(async (t) => {
+            if (/imgur\.com\/a\//.test(t)) {
+                // timestamp is imgur album
+                let hash = t.split('/').pop();
+                let resImages = await fetch(`https://api.imgur.com/3/album/${hash}/images`, {
+                    headers: {
+                        Authorization: 'Client-ID 18506a441ae08ca',
+                    },
+                });
+                if (!resImages.ok) {
+                    console.warn(`Invalid Imgur album! Cannot resolve ${t}`);
+                    return [];
+                }
+                let jsonImages = await resImages.json();
+                return jsonImages.data.map((img) => img.link);
+            } else if (/imgur\.com\/gallery\//.test(t)) {
+                // timestamp is imgur gallery
+                let hash = t.split('/').pop();
+                let resImages = await fetch(`https://api.imgur.com/3/gallery/${hash}`, {
+                    headers: {
+                        Authorization: 'Client-ID 18506a441ae08ca',
+                    },
+                });
+                if (!resImages.ok) {
+                    console.warn(`Invalid Imgur gallery! Cannot resolve ${t}`);
+                    return [];
+                }
+                let jsonImages = await resImages.json();
+                return jsonImages.data.images.map((img) => img.link);
+            } else if (/imgur\.com\/[A-Za-z0-9]*$/.test(t)) {
+                // timestamp is imgur photo
+                t = `https://i.imgur.com/${t.split('/').pop()}.jpg`;
+            }
+            return t;
+        })
+    );
+
+    // flatten and remove non-images
+    pictures = pictures.flat().filter((t) => {
+        return /\.(png|jpg|jpeg|gif|webp)\/?$/.test(t);
+    });
+
+    return pictures;
+}
+
+export default { getPictures };
--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -37,75 +37,14 @@
 </template>
 
 <script>
+import analyser from 'src/util/analyser';
+
 import Btn from 'src/components/Btn.vue';
 import RegionPicker from 'src/components/RegionPicker.vue';
 import Search from 'src/components/Search.vue';
 import Tabs from 'src/components/Tabs.vue';
 import TitleBar from 'src/components/TitleBar.vue';
 import ThemePicker from 'src/components/ThemePicker.vue';
-
-async function pictures(post) {
-    if (!post.selftext) return [];
-
-    // get links in post and avoid duplicates
-    let pictures = [
-        ...new Set(
-            [
-                ...post.selftext.matchAll(
-                    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]*\.[a-zA-Z0-9]*\b([-a-zA-Z0-9@:%_+.~#/]*)/g
-                ),
-            ].map((res) => res[0])
-        ),
-    ];
-
-    // cancel if no pictures
-    if (!pictures) return {};
-
-    pictures = await Promise.all(
-        pictures.map(async (t) => {
-            if (/imgur\.com\/a\//.test(t)) {
-                // timestamp is imgur album
-                let hash = t.split('/').pop();
-                let resImages = await fetch(`https://api.imgur.com/3/album/${hash}/images`, {
-                    headers: {
-                        Authorization: 'Client-ID 18506a441ae08ca',
-                    },
-                });
-                if (!resImages.ok) {
-                    console.warn(`Invalid Imgur album! Cannot resolve ${t}`);
-                    return [];
-                }
-                let jsonImages = await resImages.json();
-                return jsonImages.data.map((img) => img.link);
-            } else if (/imgur\.com\/gallery\//.test(t)) {
-                // timestamp is imgur gallery
-                let hash = t.split('/').pop();
-                let resImages = await fetch(`https://api.imgur.com/3/gallery/${hash}`, {
-                    headers: {
-                        Authorization: 'Client-ID 18506a441ae08ca',
-                    },
-                });
-                if (!resImages.ok) {
-                    console.warn(`Invalid Imgur gallery! Cannot resolve ${t}`);
-                    return [];
-                }
-                let jsonImages = await resImages.json();
-                return jsonImages.data.images.map((img) => img.link);
-            } else if (/imgur\.com\/[A-Za-z0-9]*$/.test(t)) {
-                // timestamp is imgur photo
-                t = `https://i.imgur.com/${t.split('/').pop()}.jpg`;
-            }
-            return t;
-        })
-    );
-
-    // flatten and remove non-images
-    pictures = pictures.flat().filter((t) => {
-        return /\.(png|jpg|jpeg|gif|webp)\/?$/.test(t);
-    });
-
-    return pictures;
-}
 
 export default {
     props: {
@@ -159,7 +98,7 @@ export default {
                 posts.map(async (post) => ({
                     regions: /\[\w+(-\w+)?]/.exec(post.data.title)[0].slice(1, -1).split('-'),
                     title: post.data.title.split('[H]')[1].split('[W]')[options.flair === 'buying' ? 1 : 0].trim(),
-                    pictures: options.flair !== 'buying' ? await pictures(post.data) : [],
+                    pictures: options.flair !== 'buying' ? await analyser.getPictures(post.data.selftext) : [],
                     href: post.data.url,
                 }))
             );
